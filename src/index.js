0b9ab607061c83a75b339bb1d4b0cac602072660
SebQuenet <seb.quenet@gmail.com>
2019-07-21 10:16:03 +0200

    Initial app setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* eslint-disable */
 ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-enable */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
